fff269c177bedb37b0fd9bc2814cd3029529a01e
Jozana <phumzilejozana0@gmail.com>
2023-03-28 05:41:11 -0700

    2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @i: string
+ * Return: the length of a string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	
+	i = strlen(*s)
+
+	return (i);
+}
