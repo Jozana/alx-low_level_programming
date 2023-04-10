a9b0807414676f4979ae2539409f413c6df47246
Jozana <phumzilejozana0@gmail.com>
2023-04-10 13:59:28 -0700

    0. A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - checks if input is lowercase
+ * @c: variable to determine if input is lowercase
+ * main - uses built in function
+ * Return: 0 if succesful
+ */
+
+int _islower(int c)
+{
+		return (c >= 'a' && c <= 'z');
+}
+
