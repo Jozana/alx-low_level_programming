eb19c05017a6553cac31112369fd96e042c78e07
Jozana <phumzilejozana0@gmail.com>
2023-04-20 00:28:36 -0700

    3. A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,6 @@
 #ifndef CALC_H
 #define CALC_H
+#include <stdlib.h>
 
 /**
  * struct op - Struct op
@@ -7,12 +8,10 @@
  * @op: The operator
  * @f: The function associated
  */
-
 typedef struct op
 {
 	char *op;
 	int (*f)(int a, int b);
-
 } op_t;
 
 int op_add(int a, int b);
