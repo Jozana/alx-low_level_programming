60708d3d228fa7572a5f5074eb403f7fadffaf9b
Jozana <phumzilejozana0@gmail.com>
2023-05-03 11:41:56 -0700

    5. Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,13 +10,13 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *current;
+	listint_t *hol;
 
 	while (*head != NULL)
 	{
-	current = *head;
+	hol = *head;
 	*head = (*head)->next;
-	free(current);
+	free(hol);
 	}
 
 	*head = NULL;
